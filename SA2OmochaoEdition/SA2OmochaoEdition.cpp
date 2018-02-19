3061c10cf3c2eaa4704ddaf96664233a70271281
MainMemory <mainmemory@yahoo.com>
2018-02-19 14:38:25 -0600

    Adding option to replace dropped rings with Omochao.

--- SA2OmochaoEdition/SA2OmochaoEdition.cpp
+++ SA2OmochaoEdition/SA2OmochaoEdition.cpp
@@ -53,6 +53,145 @@ void RingToOmochao(ObjectMaster *obj)
 	Omochao_Main(obj);
 }
 
+// void __usercall sub_4273B0(NJS_VECTOR *a1@<eax>, NJS_VECTOR *a2@<edx>, float *a3@<ecx>)
+void *sub_4273B0_ptr = (void*)0x4273B0;
+void sub_4273B0(NJS_VECTOR *a1, NJS_VECTOR *a2, float *a3)
+{
+	__asm
+	{
+		mov eax, [a1]
+		mov edx, [a2]
+		mov ecx, [a3]
+		call sub_4273B0_ptr
+	}
+}
+
+// void __usercall sub_4274E0(float *a1@<ebx>, signed int a2)
+void *sub_4274E0_ptr = (void*)0x4274E0;
+void sub_4274E0(float *a1, signed int a2)
+{
+	__asm
+	{
+		mov ebx, [a1]
+		push [a2]
+		call sub_4274E0_ptr
+		add esp, 4
+	}
+}
+
+// void __usercall sub_427680(float *a1@<ebx>, signed int a2)
+void *sub_427680_ptr = (void*)0x427680;
+void sub_427680(float *a1, signed int a2)
+{
+	__asm
+	{
+		mov ebx, [a1]
+		push [a2]
+		call sub_427680_ptr
+		add esp, 4
+	}
+}
+
+// void __usercall(int a1@<esi>, int a2, char a3, char a4)
+static const void *const PlaySoundProbablyPtr = (void*)0x437260;
+static inline void PlaySoundProbably(int a1, int a2, int a3, int a4)
+{
+	__asm
+	{
+		push [a4]
+		push [a3]
+		push [a2]
+		mov esi, [a1]
+		call PlaySoundProbablyPtr
+		add esp, 12
+	}
+}
+
+short maxringloss;
+VoidFunc(sub_429000, 0x429000);
+VoidFunc(sub_429710, 0x429710);
+FastcallFunctionPointer(float, sub_42AAB0, (Angle ang), 0x42AAB0);
+FastcallFunctionPointer(float, sub_42AC30, (Angle ang), 0x42AC30);
+FunctionPointer(void *, AllocateEntityData1, (), 0x470B40);
+DataPointer(float*, dword_1A557FC, 0x1A557FC);
+DataPointer(Angle, GravityAngle_X, 0x1DE949C);
+DataPointer(NJS_VECTOR, Gravity, 0x1DE94A0);
+DataPointer(Angle, GravityAngle_Z, 0x1DE94AC);
+void __cdecl DropOmochao(int playerNum)
+{
+	double v3 = (double)rand();
+	int id = playerNum & 1;
+	short rings = RingCount[id];
+	if (rings > 0)
+	{
+		if (rings > maxringloss)
+			rings = maxringloss;
+		AddRings(playerNum, -rings);
+		CharObj1 *v13 = MainCharObj1[playerNum];
+		int v5 = 0;
+		do
+		{
+			ObjectMaster *scatterObject = LoadObject(Omochao_Main, 2, "ObjectMessenger");
+			if (scatterObject)
+			{
+				CharObj1 *v7 = (CharObj1*)AllocateEntityData1();
+				if (v7)
+				{
+					scatterObject->Data1 = v7;
+					void *v10 = AllocateEntityData2();
+					if (v10)
+					{
+						scatterObject->field_38 = v10;
+						v7->Position.x = v13->Position.x;
+						v7->Position.y = v13->Position.y;
+						v7->Position.z = v13->Position.z;
+						v7->Rotation.y = (signed int)(((double)(v5 / rings) + v3 * 0.000030517578125 * 360.0) * 182.0444488525391);
+						NJS_VECTOR v61;
+						v61.y = 1.8f;
+						v61.x = sub_42AAB0(scatterObject->Data1->Rotation.y) * 0.4000000059604645f;
+						v61.z = sub_42AC30(scatterObject->Data1->Rotation.y) * 0.4000000059604645f;
+						sub_429710();
+						float *v20 = dword_1A557FC;
+						if (dword_1A557FC)
+						{
+							memset(dword_1A557FC, 0, 0x30u);
+							*v20 = 1.0;
+							v20[5] = 1.0;
+							v20[10] = 1.0;
+						}
+						if (GravityAngle_X)
+						{
+							sub_427680(v20, GravityAngle_X);
+						}
+						if (GravityAngle_Z)
+						{
+							sub_4274E0(v20, GravityAngle_Z);
+						}
+						sub_4273B0(&v61, (NJS_VECTOR*)scatterObject->field_38, v20);
+						sub_429000();
+						scatterObject->Data1->Scale.x = (float)(rand() % StageMessageCount); // select a random hint from the level's hint message file
+						scatterObject->Data1->Scale.y = 15;
+						scatterObject->Data1->Scale.z = 0;
+					}
+					else
+						DeleteObject_(scatterObject);
+				}
+				else
+					DeleteObject_(scatterObject);
+			}
+			v5 += 360;
+			--rings;
+		} while (rings);
+		PlaySoundProbably(32788, 0, 0, 0);
+		return;
+	}
+	char v9 = MainCharObj2[playerNum]->CharID;
+	if (v9 != Characters_MechEggman && v9 != Characters_MechTails)
+	{
+		KillPlayer(playerNum);
+	}
+}
+
 DataPointer(char **, StageMessageList, 0x1AEFD6C);
 DataPointer(char **, SysMessageList, 0x1AEFD70);
 DataPointer(int, SysMessageCount, 0xB5D204);
@@ -120,6 +259,11 @@ extern "C"
 			srand(_time32(nullptr));
 			WriteJump((void*)0x6C0F80, RingToOmochao); // turn rings into Omochao
 			WriteData((short*)0x6C605B, (short)0xE990); // prevent ring groups from crashing the game
+		}
+		if (settings->getBool("", "ReplaceDroppedRings"))
+		{
+			WriteJump(DropRings, DropOmochao);
+			maxringloss = settings->getInt("", "MaxDroppedOmochao", 20);
 		}
 		delete settings;
 	}
